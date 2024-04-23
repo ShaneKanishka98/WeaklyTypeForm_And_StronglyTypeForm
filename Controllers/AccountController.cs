94b1a766da3a3a63b4051faf9fc720c79f0629e8
ShaneKanishka98 <shanekanishka1998@gmail.com>
2024-04-23 13:28:57 +0530

    Demo of Weakly and Strongly Type Forms in C sharp

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using coreFormsAndValidations.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coreFormsAndValidations.Controllers
+{
+    public class AccountController : Controller
+    {
+        // WEAKLY TYPE FORM
+        public IActionResult WeaklyTypedLogin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LoginPost(string username, string password)
+        {
+            ViewBag.Username = username;
+            ViewBag.Password = password;
+            return View();
+        }
+
+        // STRINGLY TYPE FORM
+        public IActionResult StronglyTypedLogin() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LoginSuccess(LoginViewModel login)
+        {
+            ViewBag.Username = login.Username;
+            ViewBag.Password = login.Password;
+            return View();  
+        }
+
+    }
+}
